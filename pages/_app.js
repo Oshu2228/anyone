7cdd8fc852578a288005fe33a4fb6ad9e49f1623
Shunsuke Ono <ono0112sf@gmail.com>
2022-02-16 19:12:39 +0900

    #1 recoil,recoil-persist,chakra-uiの導入

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+<RecoilRoot>
+  <ChakraProvider>
+  <Component {...pageProps} />
+  </ChakraProvider>
+</RecoilRoot>
+  )
 }
 
 export default MyApp
